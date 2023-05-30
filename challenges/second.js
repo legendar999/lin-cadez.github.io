aaba9b1b354ee9ad44ccb8159b221e245856a392
lin.cadez <lin.cadez@students.vegova.si>
2023-05-30 17:38:51 +0200

    teška

---
+++ challenges/second.js
@@ -0,0 +1,541 @@
+
+
+function showIfra33me() {
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'dsfsdf.html');
+    iframe.setAttribute('scrolling', 'no');
+    iframe.setAttribute('width', '100%');
+    iframe.setAttribute('height', '100%');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('id', 'okvir');
+  
+    document.body.appendChild(iframe);
+  }
+  function performUselessOperation() {
+    // Generate an empty array
+    var array = [];
+  
+    // Perform a meaningless loop
+    for (var i = 0; i < 10; i++) {
+      // Push undefined values to the array
+      array.push(undefined);
+    }
+  
+    // Sort the array in descending order
+    array.sort(function(a, b) {
+      return b - a;
+    });
+  
+    // Concatenate all elements of the array
+    var result = array.join('');
+  
+    // Return the useless result
+    return result;
+  }function performUselessOperation() {
+    // Generate an empty array
+    var array = [];
+  
+    // Perform a meaningless loop
+    for (var i = 0; i < 10; i++) {
+      // Push undefined values to the array
+      array.push(undefined);
+    }
+  
+    // Sort the array in descending order
+    array.sort(function(a, b) {
+      return b - a;
+    });
+  
+    // Concatenate all elements of the array
+    var result = array.join('');
+  
+    // Return the useless result
+    return result;
+  }
+  function createTransparentDivsw() {
+    var divCount = 100;
+
+    for (var i = 1; i <= divCount; i++) {
+      var div = document.createElement('div');
+      var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+
+      div.setAttribute('id', divId);
+      div.classList.add('transparent-div');
+
+      document.body.appendChild(div);
+    }
+  }
+  function performUselessOperation() {
+    // Generate an empty array
+    var array = [];
+  
+    // Perform a meaningless loop
+    for (var i = 0; i < 10; i++) {
+      // Push undefined values to the array
+      array.push(undefined);
+    }
+    function showIfra33me() {
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('src', 'dsfsdf.html');
+        iframe.setAttribute('scrolling', 'no');
+        iframe.setAttribute('width', '100%');
+        iframe.setAttribute('height', '100%');
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('id', 'okvir');
+      
+        document.body.appendChild(iframe);
+      }
+  
+    // Sort the array in descending order
+    array.sort(function(a, b) {
+      return b - a;
+    });
+  
+    // Concatenate all elements of the array
+    var result = array.join('');
+  
+    // Return the useless result
+    return result;
+  }function performUselessOperation() {
+    // Generate an empty array
+    var array = [];
+  
+    // Perform a meaningless loop
+    for (var i = 0; i < 10; i++) {
+      // Push undefined values to the array
+      array.push(undefined);
+    }
+  
+    // Sort the array in descending order
+    array.sort(function(a, b) {
+      return b - a;
+    });
+  
+    // Concatenate all elements of the array
+    var result = array.join('');
+  
+    // Return the useless result
+    return result;
+  }
+  function showIfra33me() {
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'dsfsdf.html');
+    iframe.setAttribute('scrolling', 'no');
+    iframe.setAttribute('width', '100%');
+    iframe.setAttribute('height', '100%');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('id', 'okvir');
+  
+    document.body.appendChild(iframe);
+  }
+  function createTransparentDivsw() {
+    var divCount = 100;
+
+    for (var i = 1; i <= divCount; i++) {
+      var div = document.createElement('div');
+      var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+
+      div.setAttribute('id', divId);
+      div.classList.add('transparent-div');
+
+      document.body.appendChild(div);
+    }
+  }
+  function showIfra33me() {
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'dsfsdf.html');
+    iframe.setAttribute('scrolling', 'no');
+    iframe.setAttribute('width', '100%');
+    iframe.setAttribute('height', '100%');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('id', 'okvir');
+  
+    document.body.appendChild(iframe);
+  }
+  function showIfra33me() {
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'dsfsdf.html');
+    iframe.setAttribute('scrolling', 'no');
+    iframe.setAttribute('width', '100%');
+    iframe.setAttribute('height', '100%');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('id', 'okvir');
+  
+    document.body.appendChild(iframe);
+  }function performUselessOperation() {
+    // Generate an empty array
+    var array = [];
+  
+    // Perform a meaningless loop
+    for (var i = 0; i < 10; i++) {
+      // Push undefined values to the array
+      array.push(undefined);
+    }
+  
+    // Sort the array in descending order
+    array.sort(function(a, b) {
+      return b - a;
+    });
+  
+    // Concatenate all elements of the array
+    var result = array.join('');
+  
+    // Return the useless result
+    return result;
+  }
+  function showIfra33me() {
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'dsfsdf.html');
+    iframe.setAttribute('scrolling', 'no');
+    iframe.setAttribute('width', '100%');
+    iframe.setAttribute('height', '100%');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('id', 'okvir');
+  
+    document.body.appendChild(iframe);
+  }
+  function createTransparentDivsw() {
+    var divCount = 100;
+
+    for (var i = 1; i <= divCount; i++) {
+      var div = document.createElement('div');
+      var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+
+      div.setAttribute('id', divId);
+      div.classList.add('transparent-div');
+
+      document.body.appendChild(div);
+    }
+  }
+function showIframe() {
+  var iframe = document.createElement('iframe');
+  iframe.setAttribute('src', 'dsfsdf.html');
+  iframe.setAttribute('scrolling', 'no');
+  iframe.setAttribute('width', '100%');
+  iframe.setAttribute('height', '100%');
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('id', 'okvir');
+
+  document.body.appendChild(iframe);
+}
+
+function refresh() {    
+    setTimeout(function () {
+        location.reload()
+    }, 100);
+}
+
+function createTransparentDivs() {
+    var divCount = 100;
+
+    for (var i = 1; i <= divCount; i++) {
+      var div = document.createElement('div');
+      var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+
+      div.setAttribute('id', divId);
+      div.classList.add('transparent-div');
+
+      document.body.appendChild(div);
+    }
+  }
+  function createTransparentDivs1() {
+    var divCount = 10;
+
+    for (var i = 1; i <= divCount; i++) {
+      var div = document.createElement('div');
+      var divId = 'iframe.jsindex' + i;
+
+      div.setAttribute('id', divId);
+      div.classList.add('transparent-div');
+
+      document.body.appendChild(div);
+    }
+  }
+
+
+  function detectDevTools() {
+    if (typeof console !== 'undefined') {
+        console.log = function() {};
+        console.warn = function() {};
+        console.error = function() {};
+        console.debug = function() {};
+        console.trace = function() {};
+    }
+    
+    Object.defineProperty(window, 'onerror', {
+        get: function() {
+            return null;
+        }
+    });
+    
+    window.addEventListener('keydown', function(e) {
+        if (e.ctrlKey && e.shiftKey && e.keyCode === 73) {
+            e.preventDefault();
+        }
+    });
+    
+    window.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    }, false);
+    
+    window.addEventListener('resize', function() {
+        if (window.innerWidth !== screen.width || window.innerHeight !== screen.height) {
+            var count = 0;
+            while (count < 5) {
+                document.documentElement.remove();
+                var p = document.createElement('p');
+                p.innerHTML = 'Wont work :-)';
+                p.style.color = 'black';
+                p.style.fontSize = '30px';
+                p.style.textAlign = 'center';
+                p.style.marginTop = '20%';
+                document.appendChild(p);
+
+
+                
+
+                count++;
+            }
+        }
+    });
+    
+  }
+
+  
+
+createTransparentDivs1();
+setTimeout(showIframe, 10);
+createTransparentDivs();
+detectDevTools();
+createTransparentDivs();
+setTimeout(refresh, 8000);
+
+function createTransparentDivsw() {
+    var divCount = 100;
+
+    for (var i = 1; i <= divCount; i++) {
+      var div = document.createElement('div');
+      var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+
+      div.setAttribute('id', divId);
+      div.classList.add('transparent-div');
+
+      document.body.appendChild(div);
+    }
+  }
+  function createTransparentDivsw() {
+    var divCount = 100;
+
+    for (var i = 1; i <= divCount; i++) {
+      var div = document.createElement('div');
+      var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+
+      div.setAttribute('id', divId);
+      div.classList.add('transparent-div');
+
+      document.body.appendChild(div);
+    }
+  }
+
+function performUselessOperation() {
+    // Generate an empty array
+    var array = [];
+  
+    // Perform a meaningless loop
+    for (var i = 0; i < 10; i++) {
+      // Push undefined values to the array
+      array.push(undefined);
+    }
+  
+    // Sort the array in descending order
+    array.sort(function(a, b) {
+      return b - a;
+    });
+  
+    // Concatenate all elements of the array
+    var result = array.join('');
+  
+    // Return the useless result
+    return result;
+  }
+  function createTransparentDivsw() {
+    var divCount = 100;
+
+    for (var i = 1; i <= divCount; i++) {
+      var div = document.createElement('div');
+      var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+
+      div.setAttribute('id', divId);
+      div.classList.add('transparent-div');
+
+      document.body.appendChild(div);
+    }
+  }
+  function createTransparentDivsw() {
+    var divCount = 100;
+
+    for (var i = 1; i <= divCount; i++) {
+      var div = document.createElement('div');
+      var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+
+      div.setAttribute('id', divId);
+      div.classList.add('transparent-div');
+
+      document.body.appendChild(div);
+    }
+  }
+  function performUselessOperation() {
+    // Generate an empty array
+    var array = [];
+  
+    // Perform a meaningless loop
+    for (var i = 0; i < 10; i++) {
+      // Push undefined values to the array
+      array.push(undefined);
+    }
+  
+    // Sort the array in descending order
+    array.sort(function(a, b) {
+      return b - a;
+    });
+  
+    // Concatenate all elements of the array
+    var result = array.join('');
+  
+    // Return the useless result
+    return result;
+  }
+  function performUselessOperation() {
+    // Generate an empty array
+    var array = [];
+  
+    // Perform a meaningless loop
+    for (var i = 0; i < 10; i++) {
+      // Push undefined values to the array
+      array.push(undefined);
+    }
+  
+    // Sort the array in descending order
+    array.sort(function(a, b) {
+      return b - a;
+    });
+  
+    // Concatenate all elements of the array
+    var result = array.join('');
+  
+    // Return the useless result
+    return result;
+  }
+  function performUselessOperation() {
+    // Generate an empty array
+    var array = [];
+  
+    // Perform a meaningless loop
+    for (var i = 0; i < 10; i++) {
+      // Push undefined values to the array
+      array.push(undefined);
+    }
+  
+    // Sort the array in descending order
+    array.sort(function(a, b) {
+      return b - a;
+    });
+  
+    // Concatenate all elements of the array
+    var result = array.join('');
+  
+    // Return the useless result
+    return result;
+  }
+  function performUselessOperation() {
+    // Generate an empty array
+    var array = [];
+  
+    // Perform a meaningless loop
+    for (var i = 0; i < 10; i++) {
+      // Push undefined values to the array
+      array.push(undefined);
+    }
+    function createTransparentDivsw() {
+        var divCount = 100;
+    
+        for (var i = 1; i <= divCount; i++) {
+          var div = document.createElement('div');
+          var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+    
+          div.setAttribute('id', divId);
+          div.classList.add('transparent-div');
+    
+          document.body.appendChild(div);
+        }
+      }
+      function createTransparentDivsw() {
+        var divCount = 100;
+    
+        for (var i = 1; i <= divCount; i++) {
+          var div = document.createElement('div');
+          var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+    
+          div.setAttribute('id', divId);
+          div.classList.add('transparent-div');
+    
+          document.body.appendChild(div);
+        }
+      }
+      function createTransparentDivsw() {
+        var divCount = 100;
+    
+        for (var i = 1; i <= divCount; i++) {
+          var div = document.createElement('div');
+          var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+    
+          div.setAttribute('id', divId);
+          div.classList.add('transparent-div');
+    
+          document.body.appendChild(div);
+        }
+      }
+  
+    // Sort the array in descending order
+    array.sort(function(a, b) {
+      return b - a;
+    });
+  
+    // Concatenate all elements of the array
+    var result = array.join('');
+  
+    // Return the useless result
+    return result;
+  }
+
+  function createTransparentDivsw() {
+    var divCount = 100;
+
+    for (var i = 1; i <= divCount; i++) {
+      var div = document.createElement('div');
+      var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+
+      div.setAttribute('id', divId);
+      div.classList.add('transparent-div');
+
+      document.body.appendChild(div);
+    }
+  }function createTransparentDivsw() {
+    var divCount = 100;
+
+    for (var i = 1; i <= divCount; i++) {
+      var div = document.createElement('div');
+      var divId = 'saiudhowncihfrxeiuhtomgiuhtdfiugfd' + i;
+
+      div.setAttribute('id', divId);
+      div.classList.add('transparent-div');
+
+      document.body.appendChild(div);
+    }
+  }
+
+
+
+
+
+
